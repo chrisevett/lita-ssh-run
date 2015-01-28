1fc308801f259a1c59ebd0149f29419ce67324ea
Dan Cash <dancash04@gmail.com>
2015-01-28 13:25:47 -0600

    Committing changes before refactoring a few things.

--- lib/lita/handlers/ssh_run.rb
+++ lib/lita/handlers/ssh_run.rb
@@ -3,66 +3,64 @@ module Lita
     class SshRun < Handler
       require 'net/ssh'
 
-      route(/^run(.+)\s+on(.+)/i, :run_ssh, command: true)
+      route(/^run(.+)\s+on\s+(.+)/i, :run_ssh, command: true)
 
-      route(/^Username\s+for(.+)\s+is(.+)/i, :set_user, command: true)
+      route(/^Username\s+for\s+(.+)\s+is\s+(.+)/i, :set_user, command: true)
 
-      route(/^Password\s+for(.+)\s+is(.+)/i, :set_pass, command: true)
+      route(/^Password\s+for\s+(.+)\s+is\s+(.+)/i, :set_pass, command: true)
 
       def run_ssh(response)
 
         cmd = response.matches[0][0]
+        puts response.matches
         server = response.matches[0][1]
-        user_server = response.user.id + "_" + server
-        user_pass = redis.hgetall(user_server)
-        get_user(response, server)                      unless user_pass['usernm']
-        get_pass(response, server, user_pass['usernm']) unless user_pass['passwd']
+        id_server_user = response.user.id + "_" + server + "_user"
+        id_server_pass = response.user.id + "_" + server + "_pass"
+        usernm = redis.get(id_server_user)
+        passwd = redis.get(id_server_pass)
 
-        if user_pass['usernm'] && user_pass['passwd']
-          respone.reply("I found user and password")
+        if usernm && passwd
+
+          Net::SSH.start(server, usernm, :password => passwd) do |ssh|
+            output = ssh.exec!(cmd)
+            response.reply("```" + output + "```")
+          end
+
+        elsif usernm
+          get_user_pass(response, server, "Password")
+        elsif passwd
+          get_user_pass(response, server, "Username")
+        else
+          get_user_pass(response, server, "Username", "Password")
         end
 
-        # Net::SSH.start(server, user_pass['usernm'], :password => user_pass['passwd']) do |ssh|
-        #   output = ssh.exec!(cmd)
-        #   response.reply(output)
-        # end
-
-
 
       end
 
-      def get_user(response, server)
-        response.reply_privately("I did not find a user name for you tied to #{server}")
-        response.reply_privately("Please privately reply with your username for #{server} with the following syntax")
-        response.reply_privately("Lita, Username for #{server} is USERNM")
+
+
+      def get_user_pass(response, server, *user_pass)
+        user_pass.each do |blah|
+          response.reply_privately("No #{blah.capitalize} found for you on #{server}\nPlease privately reply with:\n`Lita, #{blah.capitalize} for #{server} is #{blah.upcase}`")
+        end
+
       end
+
 
       def set_user(response)
         server = response.matches[0][0]
         usernm = response.matches[0][1]
-        user_server = response.user.id + "_" + server
-        redis.mapped_hmset( user_server, { usernm: usernm } )
-        response.reply_privately("Set username for #{server}")
-        response.reply_privately("To change username in the future, privately reply to me with the following syntax")
-        response.reply_privately("Lita, Username for #{server} is USERNM")
-      end
-
-      def get_pass(response, server, usernm)
-        response.reply_privately("I did not find a password for #{usernm} tied to #{server}")
-        response.reply_privately("Please privately reply with your password for #{server} with the following syntax")
-        response.reply_privately("Lita, Password for #{server} is PASSWD")
+        id_server_user = response.user.id + "_" + server + "_user"
+        redis.set(id_server_user, usernm)
+        response.reply_privately("Username set")
       end
 
       def set_pass(response)
         server = response.matches[0][0]
-        user_server = response.user.id + "_" + server
-        user_pass = redis.hgetall(user_server)
-        usernm = user_pass['usernm']
-        passwd = response.matches[0][1]
-        redis.mapped_hmset( user_server, { usernm: usernm, passwd: passwd } )
-        response.reply_privately("Set password for #{server}")
-        response.reply_privately("To change password in the future, privately reply to me with the following syntax")
-        response.reply_privately("Lita, Passwd for #{server} is PASSWD")
+        passwd= response.matches[0][1]
+        id_server_pass = response.user.id + "_" + server + "_pass"
+        redis.set(id_server_pass, passwd)
+        response.reply_privately("Password set")
       end
 
 
